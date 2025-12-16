08576473320577014e32710c67434961b00a7846
sandipan-das-sd <sd901656@gmail.com>
2025-12-16 12:16:18 +0530

    feat: implement routing with react-router-dom and add authentication pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Auth from './pages/Auth'
+import Register from './pages/Register'
+import  useAuth from './context/authContext.jsx'
+import {Navigate, BrowserRouter, Routes, Route} from 'react-router-dom'
 
 export default function App() {
-  return (
-    <div>
-      app
-    </div>
-  )
+  const {token, loading}=useAuth();
+  
+  if(loading) {
+    return <div>Loading...</div>;
+  }
+
+ return (
+<BrowserRouter>
+
+<Routes>
+
+
+<Route path='/' element={!token ?<Auth/> :<Home/>}/>
+<Route path='/login' element={!token ?<Auth/> :<Home/>}/>
+<Route path='/register' element={!token ?<Register/> :<Navigate to='/'/>}/>
+</Routes>
+</BrowserRouter>
+ )
 }
